a9e290eb663e4a9db0834bb41a98f0501a42471c
Naman <Namantam1@gmail.com>
2020-05-23 21:40:07 +0530

    first commit

---
+++ core/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+# core/views.py
+
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+def login(request):
+  return render(request, 'login.html')
+
+@login_required
+def home(request):
+  return render(request, 'home.html')
---
+++ social_app/urls.py
@@ -0,0 +1,33 @@
+"""social_app URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+# social_app/urls.py
+
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from core import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+
+    #Adding social auth path
+    path('social-auth/', include('social_django.urls', namespace="social")),
+    
+    path("", views.home, name="home"),
+    path("login/", views.login, name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+]
